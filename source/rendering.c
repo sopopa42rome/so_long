249ede9b3e5cc9edc0091856e1a5236f914ae5ec
Sorin alexandru Popa <sopopa@e3r3p5.42roma.it>
2022-12-30 18:52:10 +0100

    Prove file xpm

--- source/rendering.c
+++ source/rendering.c
@@ -6,13 +6,28 @@
 /*   By: sopopa <sopopa@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/29 19:30:36 by sopopa            #+#    #+#             */
-/*   Updated: 2022/12/29 19:34:42 by sopopa           ###   ########.fr       */
+/*   Updated: 2022/12/30 18:51:31 by sopopa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-void    draw_and_render(game_vars *game)
+
+int	main(void)
 {
-    mlx_xpm_file_to_image
+	void	*mlx;
+	void	*mlx_win;
+	void    *img;
+    int     w;
+    int     h;
+
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, 800, 600, "Hello world!");
+    img = mlx_xpm_file_to_image(mlx, "../xpm/pavimento.xpm", &w, &h);
+    mlx_put_image_to_window(mlx, mlx_win, img, 0, 0);
+    mlx_destroy_image(mlx, img);
+    mlx_loop(mlx);
+    
+
+	mlx_loop(mlx);
 }
